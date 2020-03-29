be06c7b6691fad80f1293365c2a10f13b7e15d60
Karolinkas <karolin.siebert@intenthq.com>
2020-03-29 20:11:09 +0200

    Setup basic chart with example data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Tree from 'react-d3-tree';
 
-function App() {
+const myTreeData = [
+  {
+    name: 'Cornelius Schmidt',
+    attributes: {
+      'contact date:': new Date().toDateString(),
+      'confirmed case:': 'true',
+    },
+    children: [
+      {
+        name: 'Anthony Schmidt',
+        attributes: {
+          'relation:': 'son',
+          'confirmed case:': 'true',
+        },
+      },
+      {
+        name: 'Steffi Schmidt',
+        attributes: {
+          'relation:': 'daughter',
+          'confirmed case:': 'true',
+        },
+      },
+    ],
+  },
+];
+
+class NodeLabel extends React.PureComponent {
+  render() {
+    const {className, nodeData} = this.props
+    console.log(nodeData)
+    return (
+      <div className={className}>
+        <h2>{nodeData.name}</h2>
+        {Object.keys(nodeData.attributes).map(key => <p className='attribute'><span>{key}</span><span>{nodeData.attributes[key]}</span></p>)}
+        {nodeData._children && 
+          <h2>{nodeData._children.name}</h2>
+        }
+      </div>
+    )
+  }
+}
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="map-container"> 
+        <Tree data={myTreeData} collapsible={false} translate={{x: '100', y: '200'}}
+        separation={{siblings: 2, nonSiblings: 2}}
+        allowForeignObjects
+        nodeLabelComponent={{
+          render: <NodeLabel className='info-box' />,
+          foreignObjectWrapper: 
+            { width: 150,
+              height: 300
+            }
+        }} />
+      </div>
     </div>
   );
 }
